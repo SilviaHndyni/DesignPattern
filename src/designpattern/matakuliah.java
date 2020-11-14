4ef2a91f4839f6fd60abf752fe05bcc436ed2b90
Eldy Yuda Kurniawan <hardsnk11@gmail.com>
2020-11-14 10:13:38 +0800

    tambah coding matakuliah

--- src/designpattern/matakuliah.java
+++ src/designpattern/matakuliah.java
@@ -11,4 +11,13 @@ package designpattern;
  */
 public interface matakuliah {
     
+    void semester_1();
+    void semester_2();
+    void semester_3();
+    void semester_4();
+    void semester_5();
+    void semester_6();
+    void semester_7();
+    void semester_8();
+    
 }
