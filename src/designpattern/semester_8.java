85e529f02f65bf3632a041ae5ea7eda9a536eaf7
Eldy Yuda Kurniawan <hardsnk11@gmail.com>
2020-11-14 11:17:34 +0800

    Tambah class semester_8

---
+++ src/designpattern/semester_8.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpattern;
+
+/**
+ *
+ * @author ELDI
+ */
+public class semester_8 implements Command{
+       private matakuliah mk;
+    public semester_8(matakuliah mk){
+        this.mk=mk;
+    }
+
+    @Override
+    public void execute() {
+      this.mk.semester_8();}
+    
+}
