a093ba0793de8c9b59e02a5b5ce997839b722007
SilviaHndyni <silviahndyni@gmail.com>
2020-11-15 20:37:49 +0800

    tambah class guru

---
+++ src/designpattern/Guru.java
@@ -0,0 +1,14 @@
+package designpattern;
+
+public class Guru extends TemplateMethod{
+    @Override
+    public String getNama() {
+        return "Guru :";
+    }
+
+    @Override
+    public void perbedaan() {
+        System.out.println("1. Tugas utama guru adalah mendidik, mengajar, membimbing, mengarahkan, melatih, menilai, dan mengevaluasi peserta didik");
+        System.out.println("2. Wajib memiliki kualifikasi pendidikan minimum sarjana (S1) atau diplomat empat (D-IV)");
+    }
+}
